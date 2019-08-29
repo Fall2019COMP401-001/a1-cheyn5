42182b8d1771af32ecc73dfef1fb0220e6804693
Conner Heyn <33068796+cheyn5@users.noreply.github.com>
2019-08-29 18:42:43 -0400

    novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,39 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Creating a count of the number of customers
+		int count = scan.nextInt();
+		
+		// Then I am going to create an array to store the number of customers, their first initial, last name, and cost of items they purchased
+		char[] firstInitial = new char[count];
+		String[] lastName = new String[count];
+		double[] cost = new double[count];
+		
+		for (int i = 0; i<count; i++) {
+			String temp = scan.next();
+			firstInitial[i] = temp.charAt(0);
+			lastName[i] = scan.next();
+			int items = scan.nextInt();
+			
+			double sum = 0;
+			for (int j = 0; j<items; j++) {
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+				double price = scan.nextDouble();
+				sum = (price * quantity) + sum;
+			}
+			cost[i] = sum;
+		}
+		
+		for (int i = 0; i<count; i++) {
+			System.out.println(firstInitial[i] + ". " + lastName[i] + ": " + cost[i]);
+			
+		}
+		
+		
+		
+		
+		
+		
 	}
 }
